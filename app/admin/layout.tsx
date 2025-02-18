023ece0d3bb0777688d6e8cc74d290eb18ad9b93
Alaa Seada <alaaseada87@gmail.com>
2025-02-18 10:27:34 +0200

    fix props name in admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <SectionTitle title="Dashboard" className="text-xl mb-2" />
+      <SectionTitle title="Dashboard" cssClass="text-xl mb-2" />
       <div className="grid gap-y-6 md:grid-cols-12 mt-2">
         <div className="md:col-span-2">
           <SideMenu />
